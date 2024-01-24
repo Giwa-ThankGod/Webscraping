25ae36409e44cba9b2e28be6e3eb63612a8cfa0f
August <giwathankgod404@gmail.com>
2024-01-24 23:32:30 +0100

    chore: rename _init_.py to __init__.py

---
+++ sportScraping/__init__.py
@@ -0,0 +1,23 @@
+from utils import send_whatsapp_msg
+from index import free_super_tips, mrfixitstips, mighty_tips, thehardtackle, scores_24
+
+def predictions(team1, team2):
+    a = free_super_tips(team1, team2)
+    b = mighty_tips(team1, team2)
+    c = mrfixitstips(team1, team2)
+    d = scores_24(team1, team2)
+    e = thehardtackle(team1, team2)
+
+    message = f"""
+    PREDICTIONS:\n
+    freesupertips: {a},\n
+    migthytips: {b},\n
+    mrfixitstips: {c},\n
+    scores24: {d},\n
+    thehardtackle: {e},
+    """
+
+    send_whatsapp_msg("+2348055287181", message)
+
+if __name__ == "__main__":
+    predictions("Zambia","Morocco")
