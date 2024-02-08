bd67957429a3789fc08e00009a62808bef9b0403
August <giwathankgod404@gmail.com>
2024-02-08 23:06:50 +0100

    refactor(index.py): Update index.py
    Change all occurences of prediction to predictions and also makes sure that predictions are returned as a list; Remove all unneccessary print() statements.

--- pyScraper/index.py
+++ pyScraper/index.py
@@ -22,36 +22,24 @@ def free_super_tips(team1: str, team2: str):
         # Checks if the page exist on the website.
         page_not_found = soup.find(class_="Main").find_all("h1")[0]
         if page_not_found.text == "404":
-            print(False)
-            return {"title": "freesupertips", "prediction": None}
+            return {"title": "freesupertips", "predictions": None}
 
         game_date = soup.find(class_="GameBullets").find_all("li")[0:2]
         game_date = [i.text for i in game_date ] # [19:00, Today]
-        # print(game_date)
 
         prediciton_tag = soup.find_all(class_="IndividualTipPrediction")
         predicitons = []
         for prediciton in prediciton_tag:
             predicitons.append(prediciton.find("h4").text) # ['Over 2.5 Match Goals', 'AFC Bournemouth 3-1', 'Dominic Solanke To Score Anytime']
 
-        print(predicitons)
-        return {"title": "freesupertips", "prediction": predicitons}
+        return {"title": "freesupertips", "predictions": predicitons}
     except Exception as error:
-        print('Something went wrong!!!')
-        print(f'[ERROR]: {error}')
-        return {"title": "freesupertips", "prediction": None}
-
-
-
-
-free_super_tips(team1, team2)
-
+        return {"title": "freesupertips", "predictions": None}
 
 def mighty_tips(team1: str, team2: str):
     team1 = team1.lower().split(' ')[0]
     team2 = team2.lower().split(' ')[0]
     date = DATE.today().strftime('%d-%m-%Y')
-    # print(date)
 
     URL = f"https://www.mightytips.com/football-predictions/{team1}-vs-{team2}-prediction-{date}/"
     try:
@@ -61,26 +49,22 @@ def mighty_tips(team1: str, team2: str):
 
         page_not_found = soup.find(class_="mtl-page404")
         if page_not_found:
-            print(False)
-            return {"title": "mightytips", "prediction": None}
+            return {"title": "mightytips", "predictions": None}
         
         # prediction = soup.find(class_="mtl-content-block-margin mtl-content-style mtl-clearfix").find_all("h2")[1]
         # prediction = prediction.text.split(":")[1]
 
+        predictions = []
         prediction = soup.find(class_="mtl-prediction-main2__name")
-        print(prediction.text)
-        return {"title": "mightytips", "prediction": prediction.text}
+        predictions.append(prediction.text)
+        return {"title": "mightytips", "predictions": predictions}
     except Exception as error:
-        print('[Error]: ', error)
-        return {"title": "mightytips", "prediction": None}
-
-# mighty_tips(team1, team2)
+        return {"title": "mightytips", "predictions": None}
     
 def scores_24(team1: str, team2: str):
     team1 = team1.lower().replace(' ', '-')
     team2 = team2.lower().replace(' ', '-')
     date = DATE.today().strftime('%d-%m-%Y')
-    # print(date)
 
     URL = f"https://scores24.live/en/soccer/m-{date}-{team1}-{team2}-prediction"
     try:
@@ -90,21 +74,14 @@ def scores_24(team1: str, team2: str):
 
         page_not_found = soup.find(class_="sc-12l8pnt-1")
         if page_not_found:
-            print(False)
-            return {"title": "scores24", "prediction": None}
+            return {"title": "scores24", "predictions": None}
     
         prediction = soup.find(class_="sc-10cwpmp-2")
         prediction_odd = soup.find(class_="sc-10cwpmp-3")
-        print(prediction.text)
-        print(prediction_odd.text)
-        return {"title": "scores24", "prediction": [prediction.text, prediction_odd.text]}
+        return {"title": "scores24", "predictions": [prediction.text, prediction_odd.text]}
     except Exception as error:
-        print('[Error]: ', error)
-        return {"title": "scores24", "prediction": None}
+        return {"title": "scores24", "predictions": None}
     
-# scores_24(team1, team2)   
-
-
 def mrfixitstips(team1: str, team2: str):
     team1 = team1.lower().replace(' ', '-')
     team2 = team2.lower().replace(' ', '-')
@@ -119,8 +96,7 @@ def mrfixitstips(team1: str, team2: str):
         try:
             page_not_found = soup.find(class_="page-title")
             if "404" in page_not_found.text.lower():
-                print(False)
-                return {"title": "mrfixitstips", "prediction": None}
+                return {"title": "mrfixitstips", "predictions": None}
         except:
             pass
     
@@ -129,14 +105,9 @@ def mrfixitstips(team1: str, team2: str):
         for prediction in predictions_tag:
             predictions.append(prediction.find(class_="col-md-6").find("strong").text)
         
-        print(predictions)
-        return {"title": "mrfixitstips", "prediction": predictions}
+        return {"title": "mrfixitstips", "predictions": predictions}
     except Exception as error:
-        print('[Error]: ', error)
-        return {"title": "mrfixitstips", "prediction": None}
-    
-# mrfixitstips(team1, team2)
-
+        return {"title": "mrfixitstips", "predictions": None}
 
 def football_whispers(team1: str, team2: str):
     team1 = team1.lower().replace(' ', '-')
@@ -156,7 +127,6 @@ def football_whispers(team1: str, team2: str):
 
     # Extract the content after JavaScript execution
     dynamic_content = driver.page_source
-    print(dynamic_content)
 
     try:
         soup = BeautifulSoup(dynamic_content, "html.parser")
@@ -165,7 +135,7 @@ def football_whispers(team1: str, team2: str):
             page_not_found = soup.find(class_="football-container-header-single").find("h1")
             if page_not_found.text.lower() == "not found":
                 print(False)
-                return {"title": "footballwhispers", "prediction": None}
+                return {"title": "footballwhispers", "predictions": None}
         except:
             pass
     
@@ -174,7 +144,7 @@ def football_whispers(team1: str, team2: str):
         print(prediction1)
     except Exception as error:
         print('[Error]: ', error)
-        return {"title": "footballwhispers", "prediction": None}
+        return {"title": "footballwhispers", "predictions": None}
     
 # football_whispers(team1, team2)
     
@@ -182,7 +152,6 @@ def thehardtackle(team1: str, team2: str) -> dict:
     team1 = team1.lower().replace(' ', '-')
     team2 = team2.lower().replace(' ', '-')
     date = DATE.today().strftime('%Y/%m/%d')
-    # print(date)
 
     URL = f"https://thehardtackle.com/round-up/{date}/{team1}-vs-{team2}-preview-and-prediction/"
 
@@ -194,16 +163,12 @@ def thehardtackle(team1: str, team2: str) -> dict:
         try:
             page_not_found = soup.find(class_="jeg_archive_title")
             if page_not_found.text.lower() == 'page not found':
-                print(False)
-                return {"title": "thehardtackle", "prediction": None}
+                return {"title": "thehardtackle", "predictions": None}
         except:
             pass
     
         predictions_tag = soup.find(class_="content-inner").find_all("h4")[-1]
-        print(predictions_tag.text)
-        return {"title": "thehardtackle", "prediction": predictions_tag.text}
+        return {"title": "thehardtackle", "predictions": [predictions_tag.text]}
     except Exception as error:
-        print('[Error]: ', error)
-        return {"title": "thehardtackle", "prediction": None}
-    
-# thehardtackle('Leeds United', 'Norwich City')
+        return {"title": "thehardtackle", "predictions": None}
+
